908ba4fafda8dd37ac12954ccf5047e0200d0104
avii <avii@kali>
2025-02-14 18:47:33 +0545

    Removed all files except .git

--- model_inference.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Load the CSV file
-file_path = 'data/test_enriched_logs.csv'
-data = pd.read_csv(file_path)
-
-# Display the headers (column names)
-print("Column names in the CSV file:")
-print(data.columns.tolist())
